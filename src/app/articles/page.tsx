0cbcebb2ebc33ad7f3ae508c8f8484200a528d1d
Nils Hartmann <nils@nilshartmann.net>
2025-02-22 23:13:28 +0100

    03: Daten laden

--- src/app/articles/page.tsx
+++ src/app/articles/page.tsx
@@ -1,9 +1,17 @@
-import { H1 } from "@/components/Heading";
+import ArticleCard from "@/components/ArticleCard";
+import ArticleListGrid from "@/components/articlelistpage/ArticleListGrid";
+import { fetchArticleList } from "@/queries/queries";
 
-export default function ArticleListPage() {
+export default async function ArticleListPage() {
+  const articleList = await fetchArticleList();
+
   return (
-    <div>
-      <H1>Hello Articles!</H1>
+    <div className={"container mx-auto"}>
+      <ArticleListGrid>
+        {articleList.articles.map((a) => (
+          <ArticleCard key={a.id} article={a} />
+        ))}
+      </ArticleListGrid>
     </div>
   );
 }
