5ebfd877d3011cf22300e9c852b3f2802dbe1b79
Nils Hartmann <nils@nilshartmann.net>
2025-11-10 14:26:28 +0100

    Revert "Masking"
    This reverts commit 2d8f4f316237f7c5affe2cc3eda8a316704ad0a4.

--- src/apollo-client.d.ts
+++
@@ -1,8 +0,0 @@
-// This import is necessary to ensure all Apollo Client imports
-// are still available to the rest of the application.
-import "@apollo/client";
-import type { GraphQLCodegenDataMasking } from "@apollo/client/masking";
-
-declare module "@apollo/client" {
-  interface TypeOverrides extends GraphQLCodegenDataMasking.TypeOverrides {}
-}
--- src/app/articles/page.tsx
+++ src/app/articles/page.tsx
@@ -2,17 +2,14 @@ import ArticleListGrid from "@/components/articlelistpage/ArticleListGrid";
 import gql from "graphql-tag";
 import { graphlQuery } from "@/graphql-client";
 import { ArticleListDocument } from "@/_generated-graphql-types";
-import ArticleCard, { ARTICLE_CARD_FRAGMENT } from "@/components/ArticleCard";
+import ArticleCard from "@/components/ArticleCard";
 
 const ARTICLE_LIST_QUERY = gql`
-  ${ARTICLE_CARD_FRAGMENT}
   query ArticleList {
     articleList: articles(pageSize: 6) {
       totalPages
 
       articles: results {
-        id
-        body
         ...ArticleCardFragment
       }
     }
@@ -29,15 +26,13 @@ export default async function ArticleListPage() {
     throw new Error("Konnte Daten nicht laden.");
   }
 
-  // result.data.articleList.articles[0].
-
   // ... ........ ...
 
   return (
     <div className={"ArticleListPage"}>
       <ArticleListGrid>
         {result.data.articleList.articles.map((a) => (
-          <ArticleCard key={a.id} articleId={a.id} />
+          <ArticleCard key={a.id} article={a} />
         ))}
       </ArticleListGrid>
     </div>
